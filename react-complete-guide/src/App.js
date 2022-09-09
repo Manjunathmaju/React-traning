1a155c6accab0c8d63f779e389769260dad0a5c2
Manjunathmanju <8301manju@gmail.com>
2022-09-09 17:17:32 +0530

    Passing Data via props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,64 @@
-import ExpenseItem from "./components/ExpenseItem";//
+import ExpenseItem from "./components/ExpenseItem";
+
+//lower html elements are default html element 
+//upper case html element us costume componte
+
+//spicale feature JSX invented by react team
+// if we writ more time of expenseItem then it can be reuse 
+//but it is not efficient way 
+//so, for that
+//we can make out components reuseable by using concepts called as " props " in react
+
 function App() {
-  return (//spicale feature JSX invented by react team
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
-    //lower html elements are default html element 
-    //upper case html element us costume componte
   );
 }
+
 // react is componet and component is costume html element
 // react is work as all costume components to normal js code
 export default App;
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -2,17 +2,17 @@
 //starting letter should by in captial letter
 //A component is a just a Js function, a spical function
 
+// Again the file name should be same as file name
 import './ExpenseItem.css'
-function ExpenseItem() {// Again the file name should be same as file name
-    const expenseDate = new Date(2021, 3, 8);
-    const expenseTitle = 'car Insurance';
-    const expenseAmount = 300000;
+function ExpenseItem(props) {
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
-            <div className='expense-item__description'><h2>{expenseTitle}</h2></div>
-            <div className='expense-item__price'>${expenseAmount}</div>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+            </div>
+            <div className='expense-item__price'>${props.amount}</div>
         </div>
     )
 }
