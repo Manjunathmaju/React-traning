7e4d9fd8d4968776ae9bf604aa454afabb2de93d
Manjunathmanju <8301manju@gmail.com>
2022-09-09 22:48:07 +0530

    working with STATE, I wrote commends and read all those commends

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,30 +1,41 @@
-
-//starting letter should by in captial letter
-//A component is a just a Js function, a spical function
-
-// Again the file name should be same as file name
-/* <div>{props.date.toISOString('en-US',{month:'long'})}</div>
-<div>Year</div>
-<div>Day</div>
-<div>{props.date.toISOString()}</div> */
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from './UI/Card';
-
-// const ExpenseItem(props)=> {
-// works same as below function, if we put the code here.
-// }
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+    // let title = props.title;
+    const [title, setTitle] = useState(props.title);
+    //useState() returens an array, first value is value itself and second value is the updating function
+    // setTitle is not a built-in function
+    // it is user defined function 
+    // another thing is, always use first variable name, same as second variable name wilt some action adding, like line number 8th in above
 
     const clickHandler = () => {
-        console.log('Clicked!!!!!');
+        //By doing like
+        //title="updated!!"
+        //it doesn't work, we have to call the setTitle('value-to-be-update')
+
+        setTitle('updateed!!')
+
+        console.log(title);//THIS IS "NOT" UPDATING WHEN CLICKED ON THE BUTTON 
+
+        //BECAUSE React doesn't work like that
+        //React is execute only once in initial state and if we triger/call any one function after all execution completed then, it will execute only that one, NOT all the component of react
+
+        //So, to tell to run again, we have to import the libarary "import {useState} from 'react'; " and useState is a function avalibe in react
+        //who do we use the function?
+        //it will call or initilsion our function once again.
+        //Inside the our function "NOT OUTSIDE OF FUNCTION" we just call useState and it is a one-of react HOOKS.
+        //"useState()" requres a default value, 
+
     };
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
             </div>
             <div className='expense-item__price'>${props.amount}</div>
             <button onClick={clickHandler}>Change Title</button>
