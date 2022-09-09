bdd221053f90f412a502b8b5a3ca4a3cbe1b959f
Manjunathmanju <8301manju@gmail.com>
2022-09-09 15:44:14 +0530

    previous I have done hard code and now I pass values in dynamicaly and  In next I will do how to reuse this dynamic values

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -4,11 +4,15 @@
 
 import './ExpenseItem.css'
 function ExpenseItem() {// Again the file name should be same as file name
+    const expenseDate = new Date(2021, 3, 8);
+    const expenseTitle = 'car Insurance';
+    const expenseAmount = 300000;
+
     return (
         <div className='expense-item'>
-            <div>now Date()</div>
-            <div className='expense-item__description'><h2>Title</h2></div>
-            <div className='expense-item__price'>Amount</div>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'><h2>{expenseTitle}</h2></div>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
     )
 }
