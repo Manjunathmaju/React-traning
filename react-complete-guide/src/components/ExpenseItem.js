9cfe7ee9b933fa92b04ee9968de89f5622f396fd
Manjunathmanju <8301manju@gmail.com>
2022-09-09 15:33:23 +0530

    Add css for my first component and it looks good

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
+
 //starting letter should by in captial letter
 //A component is a just a Js function, a spical function
+
+import './ExpenseItem.css'
 function ExpenseItem() {// Again the file name should be same as file name
-    return (<div><h2>Expense item!</h2></div>)
+    return (
+        <div className='expense-item'>
+            <div>now Date()</div>
+            <div className='expense-item__description'><h2>Title</h2></div>
+            <div className='expense-item__price'>Amount</div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
